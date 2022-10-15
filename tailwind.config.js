11baa3add5b36b4627596aa91049c5fb817b3206
Tyler Harold <tyler@programmer.net>
2022-10-15 12:21:14 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'green': '#1CC26F',
+        'black': '#1C2129',
+        'white': '#ffffff',
+      }
+    },
+  },
+  plugins: [],
+}
